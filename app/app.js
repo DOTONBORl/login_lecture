3bc1aeb0bbf97f75f757fc9a4e8acdfa2122615b
sjkim-DTOL <sjkim@dtol.co.kr>
2024-12-22 19:18:54 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -3,11 +3,11 @@
 const express = require("express");
 const app = express();
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 뷰 엔진 설정
 app.set("view engine", "ejs");
-app.set("views", "./views"); // 뷰 파일이 저장된 디렉토리 설정 (옵션)
+app.set("views", "./src/views"); // 뷰 파일이 저장된 디렉토리 설정 (옵션)
 
 app.use("/", home); // use => 미들웨어를 등록해주는 메서드임
 
