b8b92232fae21c5b15a6ccf97ce49a2f3d992bc8
sjkim-DTOL <sjkim@dtol.co.kr>
2024-12-22 20:02:36 +0900

    DOM을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict ";
 
-console.log("Ddddddd");
-console.log("bye");
+// DOM => Document Object Model 이다
+
+const id = document.querySelector("#id"),
+  pw = document.querySelector("#pw"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+function login() {
+  // 로그인 버튼 클릭 시 id value와 pw value를 가져온다
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  // console.log(req);
+}
