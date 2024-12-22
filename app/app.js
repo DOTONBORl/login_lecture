f25f92c84b04cb6f7505cd0c130820f7cc27932c
sjkim-DTOL <sjkim@dtol.co.kr>
2024-12-22 19:30:41 +0900

    프론드 단 javascript 파일 생성

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ const home = require("./src/routes/home");
 app.set("view engine", "ejs");
 app.set("views", "./src/views"); // 뷰 파일이 저장된 디렉토리 설정 (옵션)
 
+app.use(express.static(`${__dirname}/src/public`)); // express의 static란 메서드로 정적 경로 추가
 app.use("/", home); // use => 미들웨어를 등록해주는 메서드임
 
 module.exports = app;
