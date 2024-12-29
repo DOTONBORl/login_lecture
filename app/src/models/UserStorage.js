80cd99a50738a17d60bf593e1f464e1ed18918d9
sjkim-DTOL <sjkim@dtol.co.kr>
2024-12-29 18:23:26 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,19 @@
 "use strict";
 
+const fs = require("fs").promises;
 class UserStorage {
-  static #users = {
-    id: ["aaa", "bbb", "ccc"],
-    pw: ["1111", "2222", "3333"],
-    name: ["admin", "admin2", "admin3"],
-  };
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const userInfo = Object.keys(users).reduce((newUsers, info) => {
+      newUsers[info] = users[info][idx];
+      return newUsers;
+    }, {});
+    return userInfo;
+  }
 
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -19,17 +24,16 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const userInfo = Object.keys(users).reduce((newUsers, info) => {
-      newUsers[info] = users[info][idx];
-      return newUsers;
-    }, {});
-    return userInfo;
+    return fs
+      .readFile("./src/database/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.pw.push(userInfo.pw);
